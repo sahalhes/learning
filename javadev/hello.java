56d750ae9c893ceca9a2da59be61285fb33ffaad
sahalhes <sahaldevelop@gmail.com>
2026-04-01 10:24:18 +0530

    k

---
+++ javadev/hello.java
@@ -0,0 +1,13 @@
+//jshell
+/* JDK = jvm + jre
+jvm platform independent , jvm understands byte code. first file need to have main method. 
+WORA
+
+*/
+class hello
+{
+    public static void main(String a[])
+    {
+        System.out.println("Hi");
+    }
+}
